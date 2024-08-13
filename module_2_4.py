7cf2a58bf0ba228a2be7053619c1ef89ac75e8df
saa1986 <1986zip@mail.ru>
2024-08-13 21:39:26 +0500

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,24 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]  # Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Создайте пустые списки primes и not_primes
+primes = []
+not_primes = []
+# При помощи цикла for переберите список numbers.
+for number in numbers:
+    # Пропускаем числа меньше 2, т.к. они не являются ни простыми, ни составными
+    if number < 2:
+        continue
+        # Напишите ещё один цикл for (вложенный), где будут подбираться делители для числа из 1ого цикла.
+        # Отметить простоту числа можно переменной is_prime, записав в неё занчение True перед проверкой.
+    is_prime = True
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            is_prime = False
+            break
+        # В процессе проверки на простоту записывайте числа из списка numbers в списки primes и not_primes
+    if is_prime:
+        primes.append(number)
+    else:
+        not_primes.append(number)
+# Выведите списки primes и not_primes на экран(в консоль).
+print("Primes: ", primes)
+print("Not primes: ", not_primes)
